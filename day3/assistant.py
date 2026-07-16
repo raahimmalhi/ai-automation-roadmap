cca1cfabf5430c5406edd47ae1ae02f6e7e284c0
raahim malhi <raahim.zulfiqar.5@gmail.com>
2026-07-17 01:37:54 +0500

    task completed

--- day3/assistant.py
+++ day3/assistant.py
@@ -1,13 +1,14 @@
-
-print("AI ASSISTANT \n Hi, I am your AI assistant. How can I help you today? \n 1)BUY PRODUCT \n 2)GET SUPPORT \n3)TALK TO HUMAN")
-choice = input("Enter your choice: ")
-if choice == "1":
-    print("You have chosen to buy a product. Please visit our website to browse our products.")
-elif choice == "2":
-    print("You have chosen to get support. Please visit our support page for assistance.")
-elif choice == "3":
-    print("You have chosen to talk to a human. Please wait while we connect you to a customer service representative.")
-elif choice == "4":
-    print("succesfully exited")
-else:
-    print("Invalid choice. Please enter a valid option.")
+while True:
+    print("AI ASSISTANT \n Hi, I am your AI assistant. How can I help you today? \n 1)BUY PRODUCT \n 2)GET SUPPORT \n3)TALK TO HUMAN \n4)EXIT")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        print("You have chosen to buy a product. Please visit our website to browse our products.")
+    elif choice == "2":
+        print("You have chosen to get support. Please visit our support page for assistance.")
+    elif choice == "3":
+        print("You have chosen to talk to a human. Please wait while we connect you to a customer service representative.")
+    elif choice == "4":
+        print("succesfully exited")
+        break
+    else:
+        print("Invalid choice. Please enter a valid option.")
---
+++ day4/customer_messages.py
@@ -0,0 +1,4 @@
+names = ["joe", "sally", "bob"]
+
+for index, name in enumerate(names):
+    print(f"Welcome {name}")
